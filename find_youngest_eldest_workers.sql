a5bc720de835f3612b95c7a9d846ec5acf30fed8
AlexBosenko <132894195+AlexBosenko@users.noreply.github.com>
2023-09-30 09:42:26 +0300

    Update find_youngest_eldest_workers.sql

--- find_youngest_eldest_workers.sql
+++ find_youngest_eldest_workers.sql
@@ -1,11 +1,11 @@
-SELECT 'YOUNGEST' AS type, w.name, w.birthday AS birthday
+SELECT 'YOUNGEST' AS type, w.name, w.birthday
 FROM worker AS w
 WHERE w.birthday IN (
     SELECT MAX(birthday)
     FROM worker
 )
 UNION
-SELECT 'ELDEST', w.name, w.birthday AS birthday
+SELECT 'ELDEST', w.name, w.birthday
 FROM worker AS w
 WHERE w.birthday IN (
     SELECT MIN(birthday)
